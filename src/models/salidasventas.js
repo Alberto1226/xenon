9cb5fa4ffe1d9781c2aeb495b353b321adbe9c92
AngelResendizCGO <angel.jrl@hotmail.com>
2025-07-02 13:23:12 -0600

    Agregar modelo de SalidasVentas con referencias a Rutas, Carritos y Usuario, incluyendo campos para productos, fecha de salida y estado.

---
+++ src/models/salidasventas.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+import {Rutas} from './rutas';
+import {Carritos} from './carrito';
+import {Usuario} from './usuario';
+
+var schema = new Schema({
+    id_ruta: { type: Schema.Types.ObjectId, ref: 'Rutas' }, // Referencia al _id de la colección Rutas
+    id_carritos: { type: Schema.Types.ObjectId, ref: 'Carritos' }, // Referencia al _id de la colección Carritos
+    id_usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' }, // Referencia al _id de la colección Usuario
+    Productos: { type: Array, default: [] }, // Arreglo de productos que se incluyen en la salida
+    fecha_salina: { type: Date, default: Date.now }, // Fecha en la que se realizó la salida
+    folio_salida: { type: String },
+    status: { type: String, default: 'Activa' }, // Estado de la salida (Activa, Proceso, Terminada)
+});
+
+module.exports = mongoose.model('SalidasVentas', schema);

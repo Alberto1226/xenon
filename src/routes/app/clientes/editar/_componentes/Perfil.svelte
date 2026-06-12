188bffd2bc9690274dabd11c06af9e28fd0ed81a
AngelResendizCGO <angel.jrl@hotmail.com>
2026-06-12 11:35:44 -0600

    cambio en como se pone el descuento del cliente, de perfiles a solo porcentajes

--- src/routes/app/clientes/editar/_componentes/Perfil.svelte
+++ src/routes/app/clientes/editar/_componentes/Perfil.svelte
@@ -3,16 +3,14 @@
   import {formato_precio,usuario_db} from './../../../../stores';
   export var perfil;
   
-
-
-const perfiles_lista=[  
-{mostrar:"Elite",perfil:"Elite"},
-{mostrar:"Distribuidor",perfil:"Distribuidor"},
-{mostrar:"Mayoreo",perfil:"Mayoreo"},
-{mostrar:"Menudeo",perfil:"Menudeo"},
-{mostrar: "Público en general",perfil:"Público en general"}
-]
-  var lista_visible=true
+  const perfiles_lista = [];
+  for (let i = 0; i <= 50; i += 5) {
+    perfiles_lista.push({ mostrar: `${i}%`, perfil: "Mayoreo", porcentaje: i });
+  }
+  for (let i = 51; i <= 60; i++) {
+    perfiles_lista.push({ mostrar: `${i}%`, perfil: "Mayoreo", porcentaje: i });
+  }
+  var lista_visible = true;
 </script>
 
 <style>
@@ -20,26 +18,21 @@ const perfiles_lista=[
   overflow-y: auto;
   height: 150px;
   width: 350px;
-  
 }
 </style>
 
-
 {#if $usuario_db.rol ==='gerente' || $usuario_db.rol ==='administrador'}
-   <!-- content here -->
    <Menu origin="top left" style="width:250px;">
   <div slot="activator">
-    <Button  color={perfil.perfil==""?"red":"primary"} raised ripple={false} style="padding-right: 4px;width:100%;">
+    <Button color={perfil.porcentaje === undefined ? "red" : "primary"} raised ripple={false} style="padding-right: 4px;width:100%;">
       <i class="material-icons vertical-alineado icono_peque">
         arrow_drop_down
       </i>
-      <span>perfil : {perfil.perfil == '' ? 'pendiente...' : perfil.perfil}</span>
-
+      <span>descuento : {perfil.mostrar || (perfil.porcentaje !== undefined ? perfil.porcentaje + '%' : (perfil.perfil == '' ? 'pendiente...' : perfil.perfil))}</span>
     </Button>
   </div>
   <div class="scrollable">
   {#each perfiles_lista as item}
-    <!-- content here -->
     <Menuitem
       on:click={() => {
         perfil = item;
@@ -48,12 +41,7 @@ const perfiles_lista=[
     </Menuitem>
   {/each}
   </div>
-
-  
 </Menu>
 {:else}
-   {perfil.perfil}
+   {perfil.mostrar || (perfil.porcentaje !== undefined ? perfil.porcentaje + '%' : perfil.perfil)}
 {/if}
-
-
-
